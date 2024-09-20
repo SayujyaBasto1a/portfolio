ac965c0f4a03c2dc575b09ed3632fc6f0321593d
SayujyaBasto1a <124507889+SayujyaBasto1a@users.noreply.github.com>
2024-09-20 11:00:11 +0545

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,10 @@
+var sidemenu = document.getElementById("sideMenu");
+
+function openMenu(){
+    sidemenu.style.left = "0";
+    sidemenu.style.display = "block"; 
+}
+
+function closeMenu(){
+    sidemenu.style.left ="-100%";
+}
