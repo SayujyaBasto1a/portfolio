72bcd437e042a66ea482d1ecbacedda743fb630c
SayujyaBasto1a <124507889+SayujyaBasto1a@users.noreply.github.com>
2024-09-20 15:52:15 +0545

    updated the js, html & css

--- menu.js
+++ menu.js
@@ -1,10 +1,28 @@
 var sidemenu = document.getElementById("sideMenu");
 
-function openMenu(){
-    sidemenu.style.left = "0";
-    sidemenu.style.display = "block"; 
+function openMenu() {
+  sidemenu.style.left = "0";
+  sidemenu.style.display = "block";
 }
 
-function closeMenu(){
-    sidemenu.style.left ="-100%";
+function closeMenu() {
+  sidemenu.style.left = "-100%";
+}
+
+let mybutton = document.getElementById("topBtn");
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function toTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
